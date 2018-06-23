833126ab1e6ac6d70f71a416db87f2bae3216f6c
Rishabh Kumar Minocha <rkminoch@gwem.csclub.uwaterloo.ca>
2018-06-23 19:11:17 -0400

    First commit

---
+++ app/src/main/java/com/rishabhminocha/courseplanner/MainActivity.java
@@ -0,0 +1,119 @@
+package com.rishabhminocha.courseplanner;
+
+import android.Manifest;
+import android.app.Activity;
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Handler;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    private String selectedFilePath;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button uploadTranscript = (Button)findViewById(R.id.uploadTranscript);
+        uploadTranscript.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                showFileChooser();
+                }
+        });
+    }
+
+    private void showFileChooser() {
+        Intent intent = new Intent();
+        //sets the select file to all types of files
+        intent.setType("*/*");
+        //allows to select data and return it
+        intent.addCategory(Intent.CATEGORY_OPENABLE);
+        //starts new activity to select file and return data
+        startActivityForResult(Intent.createChooser(intent,"Choose File to Upload.."), 1);
+
+    }
+
+
+/*
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 7 && resultCode == RESULT_OK && data != null && data.getData() != null) {
+            final ProgressDialog dialog = ProgressDialog.show(MainActivity.this, "Parsing transcript", "Please wait", true);
+            dialog.show();
+            Handler handler = new Handler();
+            handler.postDelayed(new Runnable() {
+                public void run() {
+                    dialog.dismiss();
+                    Intent i1 = new Intent(MainActivity.this, UploadTranscript.class);
+                    startActivity(i1);
+                }
+            }, 3000); // delay
+        }
+    }
+    */
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(resultCode == Activity.RESULT_OK){
+            if(requestCode == 7){
+                if(data == null){
+                    //no data present
+                    return;
+                }
+                final ProgressDialog dialog = ProgressDialog.show(MainActivity.this, "Parsing transcript", "Please wait", true);
+                dialog.show();
+                Handler handler = new Handler();
+                handler.postDelayed(new Runnable() {
+                    public void run() {
+                        dialog.dismiss();
+                        Intent i1 = new Intent(MainActivity.this, UploadTranscript.class);
+                        startActivity(i1);
+                    }
+                }, 3000); // delay
+
+            }
+        }
+    }
+
+
+
+
+    protected void makeRequest() {
+        ActivityCompat.requestPermissions(this,
+                new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},
+                101);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case 101: {
+
+                if (grantResults.length == 0
+                        || grantResults[0] !=
+                        PackageManager.PERMISSION_GRANTED) {
+
+
+                } else {
+
+                }
+                return;
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/rishabhminocha/courseplanner/UploadTranscript.java
@@ -0,0 +1,17 @@
+package com.rishabhminocha.courseplanner;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by rkminoch on 22/06/18.
+ */
+
+public class UploadTranscript extends AppCompatActivity{
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_upload_transcript);
+    }
+}
