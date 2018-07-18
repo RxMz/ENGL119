6e156b652730746e27f9b076d69c6adf6f499b0f
Rishabh Kumar Minocha <rkminoch@strombola.csclub.uwaterloo.ca>
2018-07-18 17:43:25 -0400

    Dummy added

--- app/src/main/java/com/rishabhminocha/courseplanner/AddCourse.java
+++ app/src/main/java/com/rishabhminocha/courseplanner/AddCourse.java
@@ -1,8 +1,16 @@
 package com.rishabhminocha.courseplanner;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 /**
  * Created by rkminoch on 18/07/18.
@@ -13,5 +21,21 @@ class AddCourse  extends AppCompatActivity{
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_course);
+        TextView button = (TextView) findViewById(R.id.update);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final ProgressDialog dialog = ProgressDialog.show(AddCourse.this, "Parsing transcript", "Please wait", true);
+                dialog.show();
+                Handler handler = new Handler();
+                handler.postDelayed(new Runnable() {
+                    public void run() {
+                        dialog.dismiss();
+                        Intent i1 = new Intent(AddCourse.this, UploadTranscript.class);
+                        startActivity(i1);
+                    }
+                }, 5000); // delay
+            }
+        });
     }
 }
